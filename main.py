875d2d9c08c59e58dea79be12abacdf14e87cdc5
serii <seriiburduja@gmail.com>
2024-07-25 16:32:30 +0300

    feat: venv

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import os
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 service = Service(executable_path="/usr/bin/chromedriver")
 driver = webdriver.Chrome(service=service)
 
 driver.get("https://www.google.com")
+
+input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+input_element.send_keys("Hello World")
 
 time.sleep(10)
 
